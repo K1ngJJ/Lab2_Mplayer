9c411f8175c0b599de2e9e153dc7d7a0c3be2ff8
Pacheco, King JayJay A <144676325+K1ngJJ@users.noreply.github.com>
2023-09-25 13:21:02 +0800

    Update play.php

--- app/Views/play.php
+++ app/Views/play.php
@@ -1,14 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Music Management</title>
-    <!-- Include Bootstrap CSS -->
+    <title>MPlayer</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
-
 <body style="background-color: #efdecd;">
   <div class="container mt-5">
     <div class="card p-4">
@@ -21,23 +18,21 @@
         <div class="container mt-5">
           <div class="card p-4" style="background-color: #c4bdaf">
             <div class="mb-4">
-
               <?php if (isset($music_to_play)): ?>
                   <div class="mb-3">
-                      <a><strong>Title:</strong>
+                      <a><strong>Title: </strong>
                           <?= $music_to_play['title'] ?>
                       </a>
-                      <br />
-                      <a><strong>Playlist:</strong>
+                      <br /><br/>
+                      <a><strong>Playlist: </strong>
                               <?= $music_to_play['playlist'] ?>
                           </a><br><br />
 
                       <audio controls >
                           <source src="<?= base_url('uploads/' . $music_to_play['file_name']) ?>" type="audio/mpeg">
                       </audio>
-
                   </div>
-                  <a href="javascript:history.back()" class="btn btn-primary" style="background-color: #0892d0">Go Back</a>
+                  <a href="javascript:history.back()" class="btn btn-primary" style="background-color: #0892d0">Back</a>
               <?php endif; ?>
         </div>
         </div>
@@ -47,10 +42,7 @@
 </div>
 </div>
 </div>
-
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </body>
-
 </html>
