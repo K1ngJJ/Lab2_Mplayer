9c317d232386d7dfc4c4552408b5d7fab19ffb7d
Pacheco, King JayJay A <144676325+K1ngJJ@users.noreply.github.com>
2023-09-25 13:19:22 +0800

    Update CreatePlaylist.php

--- app/Views/CreatePlaylist.php
+++ app/Views/CreatePlaylist.php
@@ -1,14 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Music Management</title>
-    <!-- Include Bootstrap CSS -->
+    <title>MPlayer</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
-
 <body style="background-color: #efdecd;">
   <div class="container mt-5">
     <div class="card p-4">
@@ -16,28 +13,25 @@
     <div class="mb-4">
         <form action="<?= site_url('music/add_playlist') ?>" method="post">
             <div class="mb-3">
-                <label for="playlist_name" class="form-label">Playlist Name:</label>
-                <input type="text" name="playlist_name" class="form-control" required>
+                <label for="playlist_name" class="form-label">Playlist Name: </label>
+                <input type="text" name="playlist_name" class="form-control" placeholder="Enter Playlist Name" required>
             </div>
             <div class="container mt-2">
                   <div class="mb-1">
-            <button type="submit" class="btn btn-success" style="background-color: #319177">Add Playlist</button>
-              <span><a class="btn btn-primary" href="/music" style="background-color: #0892d0">Go back</a></span>
+            <button type="submit" class="btn btn-success" style="background-color: #319177">Create Playlist</button>
+              <span><a class="btn btn-primary" href="/music" style="background-color: #0892d0">Back</a></span>
           </div>
         </div>
         </form>
     </div>
-
-    <!-- Playlists -->
     <table class="table table-striped">
         <thead>
             <tr>
                 <th>Playlist</th>
-                <th>Action</th>
+                <th></th>
             </tr>
         </thead>
         <tbody>
-
             <?php foreach ($playlist as $playlist): ?>
                 <tr>
                     <td>
@@ -54,9 +48,7 @@
 </div>
 </div>
 </div>
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </body>
-
 </html>
